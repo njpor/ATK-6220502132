da7f4e983c56f8b99b82792c8d442866e740cdd6
njpor <nnppkk.j@gmail.com>
2021-09-29 15:02:10 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,21 +1,18 @@
 <?php
-if(isset($_GET['controller'])&&isset($_GET[action']))
+if(isset($_GET['controller'])&&isset($GET['action']))
 {
-       $controller = $_GET['controller'];
-       $action = $_GET['action'];
-
-}
-else
-{      $controller = 'pages';
-       $action = 'home';
-
+    $controller = $_GET['controller'];
+    $action = $_GET['action'];
+}else
+{
+    $controller = 'pages';
+    $action = 'home';
 }?>
-
 <html>
-<head></head>
-<body>
-     <?php echo "controller.",action = ".$action.;?>
-     <br>[<a href="">Home</a>]<br>
-     <?php require_once("routes.php");?>
-</body>
+    <head></head>
+    <body>
+        <?php echo "controller= ".$controller.",action=".$action;?>
+        <br>[<a href="">home</a>]<br>        
+        <?php require_once("routes.php");?>
+    </body>
 </html>
