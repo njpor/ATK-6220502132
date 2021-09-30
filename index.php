2180580f971713c8d8de507b2bf18a016beddd65
njpor <nnppkk.j@gmail.com>
2021-10-01 04:37:25 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head> 
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=100.0">
+    
+    <h1 style="font-family:verdana;">Premium Shop</h1>
 
+   
+</head>
+    <body>
+    <p style="font-family:courier;">I'm so ขี้เกียจ TOT</p>
+        <button ="">HOME</button>
+        <button ="">Quotation</button>
+        <button ="">Quotation Detail</button>
+
+    </body>
+</html>
