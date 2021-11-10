fb7ca56d411fb88dbec38e40937b70d5808be5f3
njpor <91516693+njpor@users.noreply.github.com>
2021-11-11 00:58:30 +0700

    Add files via upload

---
+++ db21_091/views/pages/home.php
@@ -0,0 +1,33 @@
+<!doctype html>
+<html>
+    <head>
+        <title></title>
+        <style></style>
+    </head>
+    <body>
+      <br><br>
+      <div class="main">
+            <div class="tag">ATK project</div>
+            <div class="img">
+                <img src="https://images.unsplash.com/photo-1585222515068-7201a72c4181?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=774&q=80" alt="">
+            </div>
+            <div class="content">
+                <h3>Naphut Jiamprasert</h3>
+                <h1>Homepage</h1>
+                <p>
+                    Welcome na so tired susu!
+                </p>
+            </div>
+            <div class="footer">
+                <div class="ft-l">
+                    <img src="https://images.unsplash.com/photo-1496096265110-f83ad7f96608?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1170&q=80" atl="">
+                    <p>Naphut</p>
+                </div>
+                <div class="ft-r">
+                    <p>6220502132</p>
+                </div>
+            </div>
+        </div> 
+        <br><br><br><br>
+    </body>
+</html>
